fef9a4c4e4560abcf4c7b046315f2b31f2d4a89d
delucagennaro88 <delucagennaro88@gmail.com>
2018-08-14 19:21:38 +0200

    Work in progress 2

---
+++ Movie.py
@@ -0,0 +1,13 @@
+from Folder import Folder
+
+class Movie(Folder):
+    def __init__(self, home_path, name, dir, atime, ctime, size, ext, id, url, title, year, plot, director_box, actor_id, actor_name):
+        super().__init__(home_path, name, dir, atime, ctime, size, ext)
+        self.id = id
+        self.url = url
+        self.title = title
+        self.year = year
+        self.plot = plot
+        self.director_box = director_box
+        self.actor_name = actor_name
+        self.actor_id = actor_id
--- mapTheFolder.py
+++ mapTheFolder.py
@@ -1,6 +1,6 @@
 import os, time, sys
 from bytes_converter import bytes_converter
-from Folder import Folder
+from Movie import Movie
 import imdb
 
 directory = "C:\\Users\\Utente\\Desktop\\FILM"
@@ -9,17 +9,19 @@ ia = imdb.IMDb()
 
 def search_director(movie_identifier):
     directors = movie_identifier['director']
+    director_container= {}
     for dir in directors:
         director_name = dir['name']
         director_id = dir.personID
-        print(director_name, director_id)
+        director_container.update({'DirectorName': director_name,'DirectorId': director_id})
+        return director_container
 
 def search_cast(movie_identifier):
     actors = movie_identifier['cast']
     for actor in actors:
         actor_name = actor['name']
         actor_id = actor.personID
-        print(actor_name, actor_id)
+        return actor_name, actor_id
 
 def movie_name(i):
     try:
@@ -32,11 +34,8 @@ def movie_name(i):
         movie_identifier = ia.get_movie(movie_id)
         plot = movie_identifier.get('plot', [''])[0]
         plot = plot.split('::')[0]
-        print('Info on movies\n\n\n')
-        print(title + '\n\n\n' + imdbURL + '\n\n\n' + str(movie_id) + '\n\n\n' + str(movie_year))
-        search_director(movie_identifier)
-        search_cast(movie_identifier)
-        print(plot)
+        director_box = search_director(movie_identifier)
+        actor_name, actor_id = search_cast(movie_identifier)
 
     except imdb.IMDbError as e:
         print("Probably you're not connected to Internet.  Complete error report:")
@@ -47,6 +46,10 @@ def movie_name(i):
         print('It seems that there\'s no movie with movie_id "%s"' % title)
         sys.exit(4)
 
+    movie_container = {'Title': title, 'Url': imdbURL, 'Id': movie_id, 'Year': movie_year, 'Plot': plot, 'DirectorBox': director_box, 'ActorName': actor_name, 'ActorId': actor_id}
+
+    return movie_container
+
 def save_info(directory):
     if os.path.exists(directory):
         for i in os.listdir(directory):
@@ -56,23 +59,20 @@ def save_info(directory):
             dir = os.path.join(directory, i)
 
             if os.path.isfile(dir):
-                print('è un file!')
                 ext = os.path.splitext(dir)[1]
                 size = bytes_converter(os.path.getsize(dir))
 
             elif os.path.isdir(dir):
-                print('è una cartella')
                 ext = 'directory' #qui andrebbe messa una funzione che apre la cartella e analizza i file interni
                 size = 0 #qui andrebbe messa una funzione che quantifica i file nella cartella
 
             else:
-                print('non è un file')
                 ext = 'unknown'
                 size = 0
 
             a = os.stat(os.path.join(directory, i))
 
-            collection.append(Folder(directory, i, dir, time.ctime(a.st_atime), time.ctime(a.st_ctime), size, ext))
+            collection.append(Movie(directory, i, dir, time.ctime(a.st_atime), time.ctime(a.st_ctime), size, ext, film['Id'], film['Url'], film['Title'], film['Year'], film['Plot'], film['DirectorBox'], film['ActorName'], film['ActorId']))
 
         return collection
 
@@ -84,5 +84,12 @@ def save_info(directory):
 save_info(directory)
 
 for i in collection:
-    print('Info on files\n\n\n')
-    print(i.home_path+'\n\n\n'+i.name+'\n\n\n'+i.dir+'\n\n\n'+str(i.atime)+'\n\n\n'+str(i.ctime)+'\n\n\n'+str(i.size)+'\n\n\n'+i.ext)
+    #print('\n')
+    #print('Informazioni sul file' + '\n' + i.home_path + '\n' + i.name + '\n' + i.dir + '\n' + str(i.atime) + '\n' + str(i.ctime) + '\n' + str(i.size) + '\n' + i.ext + '\n\n\n')
+    #print('Informazioni sul film' + '\n' + str(i.id) + '\n' + i.url + '\n' + i.title + '\n' + str(i.year) + '\n' + i.plot + '\n')
+
+    for value in i.director_box.values():
+        print(value)
+    #for nome_attore, id_attore in i.actor_name, i.actor_id:
+    #    print(nome_attore+ '\n' + str(id_attore) + '\n')
+    print('\n\n\n')

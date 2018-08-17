0335b4b83a9004825daa32dc1b516282186ea9ea
delucagennaro88 <delucagennaro88@gmail.com>
2018-08-17 12:34:16 +0200

    Work in progress 2

--- Movie.py
+++ Movie.py
@@ -1,8 +1,8 @@
 from Folder import Folder
 
 class Movie(Folder):
-    def __init__(self, home_path, name, dir, atime, ctime, size, ext, id, url, title, year, plot, director_box, actor_box):
-        super().__init__(home_path, name, dir, atime, ctime, size, ext)
+    def __init__(self, Folder, id, url, title, year, plot, director_box, actor_box):
+        super().__init__(Folder.home_path, Folder.name, Folder.dir, Folder.atime, Folder.ctime, Folder.size, Folder.ext)
         self.id = id
         self.url = url
         self.title = title
--- mapTheFolder.py
+++ mapTheFolder.py
@@ -1,17 +1,55 @@
 from file_manager import save_info
-import csv
-import json
+import json, os
+from Folder import Folder
+from Movie import Movie
 
 directory = "C:\\Users\\Utente\\Desktop\\FILM"
+json_file = 'cinema.json'
 
+json_dir = os.path.join(directory, json_file)
 # Qui comincia il programma
 
-collection = save_info(directory)
+if not os.path.exists(directory):
+    print('Il file non esiste!')
+    collection = save_info(directory)
 
-#json_collection = json.dumps(collection, sort_keys=True)
+    with open('cinema.json', 'w') as outfile:
+        json.dump(collection, outfile, sort_keys=True, indent=4, ensure_ascii=False)
 
-#print(json_collection)
+else:
+    print('Il file esiste!')
+    #creare funzione save_info_lite() che verifica gli aggiornamenti rispetto all'ultima versione
+    #e inserisce solo quelli
+    #il file json deve essere solo aggiornato
 
-outfile = open( 'dict.csv', 'w' )
-for key, value in sorted( collection.items() ):
-    outfile.write( str(key) + '\t' + str(value) + '\n' )
+def open_json():
+    movie_json= json.load(open('cinema.json'))
+
+    for i in movie_json.values():
+        for x in i:
+            home_path = x['Home Directory']
+            file_name = x['File Name']
+            id = x['Id'] #nome del File senza Directory
+            ext = x['Extension']
+            atime = x['Atime']
+            ctime = x['Ctime']
+            size = x['Size']
+            #folder_class = Folder(home_path, file_name, id, atime, ctime, size, ext)
+            movie_id = x['Movie Id']
+            movie_title = x['Movie Title']
+            movie_url = x['Movie Url']
+            movie_year = x['Movie Year']
+            movie_plot = x['Movie plot']
+
+            '''
+            for y in x['Movie Director']:
+                print(y['Name'])
+                print(y['Id'])
+                print('\n')
+    
+            for z in x['Movie Actor']:
+                print(z['Name'])
+                print(z['Id'])
+                print('\n')
+            '''
+            #movie_class = Movie(folder_class, movie_id, movie_url, movie_title, movie_year, movie_plot, movie_director, movie_cast)

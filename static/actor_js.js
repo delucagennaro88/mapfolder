712c2d22a221adadeb3ea5749be2ab045f97c90d
delucagennaro88 <delucagennaro88@gmail.com>
2018-11-16 18:22:59 +0100

    Work in progress 17

---
+++ static/actor_js.js
@@ -0,0 +1,21 @@
+$(".base")
+  .clone()
+  .addClass("overlay")
+  .appendTo(".landing");
+$(".cta").hover(function() {
+  $(".overlay").toggleClass("over");
+});
+
+/*--------------------
+Codepen Preview Tile
+--------------------*/
+setTimeout(function() {
+  $(".overlay")
+    .addClass("over")
+    .delay(600)
+    .queue(function() {
+      $(this)
+        .removeClass("over")
+        .dequeue();
+    });
+}, 400);
